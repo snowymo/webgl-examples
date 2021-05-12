69e1e812de2ea80bcfaa8feb81c69f5b85494b2d
Zhenyi He <snowymohehe@gmail.com>
2021-05-12 20:52:22 +0800

    :confused: cannot render plane so static cube instead

--- tutorial/ImagePlane/imagePlane.js
+++ tutorial/ImagePlane/imagePlane.js
@@ -119,7 +119,7 @@ function initBuffers(gl) {
     1.0, 1.0, -1.0,
     1.0, -1.0, -1.0,
 
-    // Top face
+    // // Top face
     -1.0, 1.0, -1.0,
     -1.0, 1.0, 1.0,
     1.0, 1.0, 1.0,
@@ -166,7 +166,7 @@ function initBuffers(gl) {
     1.0, 0.0,
     1.0, 1.0,
     0.0, 1.0,
-    // Top
+    // // Top
     0.0, 0.0,
     1.0, 0.0,
     1.0, 1.0,
@@ -319,15 +319,16 @@ function drawScene(gl, programInfo, buffers, texture, deltaTime) {
 
   mat4.translate(modelViewMatrix,     // destination matrix
     modelViewMatrix,     // matrix to translate
-    [-0.0, 0.0, -6.0]);  // amount to translate
-  mat4.rotate(modelViewMatrix,  // destination matrix
-    modelViewMatrix,  // matrix to rotate
-    cubeRotation,     // amount to rotate in radians
-    [0, 0, 1]);       // axis to rotate around (Z)
-  mat4.rotate(modelViewMatrix,  // destination matrix
-    modelViewMatrix,  // matrix to rotate
-    cubeRotation * .7,// amount to rotate in radians
-    [0, 1, 0]);       // axis to rotate around (X)
+    [-0.0, 0.0, -5.0]);  // amount to translate
+  mat4.scale(modelViewMatrix, modelViewMatrix, [1.44, 1.6, 1]);
+  // mat4.rotate(modelViewMatrix,  // destination matrix
+  //   modelViewMatrix,  // matrix to rotate
+  //   cubeRotation,     // amount to rotate in radians
+  //   [0, 0, 1]);       // axis to rotate around (Z)
+  // mat4.rotate(modelViewMatrix,  // destination matrix
+  //   modelViewMatrix,  // matrix to rotate
+  //   cubeRotation * .7,// amount to rotate in radians
+  //   [0, 1, 0]);       // axis to rotate around (X)
 
   // Tell WebGL how to pull out the positions from the position
   // buffer into the vertexPosition attribute
@@ -407,7 +408,7 @@ function drawScene(gl, programInfo, buffers, texture, deltaTime) {
 
   // Update the rotation for the next draw
 
-  cubeRotation += deltaTime;
+  // cubeRotation += deltaTime;
 }
 
 //
